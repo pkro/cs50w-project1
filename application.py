4cbf4f15af0e5899e3b5cc214b6a6ac1a7e33155
pkro <pkrombach@web.de>
2019-04-22 20:55:16 +0200

    finished and removed repetitive code in results function, show basic results

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from werkzeug.security import generate_password_hash, check_password_hash
+import datetime
 
 app = Flask(__name__)
 
@@ -87,10 +88,54 @@ def search():
     return render_template("search.html")
 
 # Book results
-# optional: add fuzzy search if no results
+# ToDo: save last search in session and prefill form
+#       add this sites avg rating / nr of reviews and goodreads avg rating / reviews to result
 @app.route("/results", methods=["POST"])
 def results():
-    pass
+    use_strict = True if request.form.get('use_strict') == '1' else False
+    sql_comparison = '=' if use_strict else 'like'
+
+    # build the where clause depending on what was filled in and fill variables for execute
+    cols = dict()
+    where_clause = []
+    for col in ['title', 'author', 'isbn']:
+        cols[col] = request.form.get(col).lower()
+        if cols[col] != '':
+            where_clause.append(f"LOWER({col}) {sql_comparison} :{col}")
+            if not use_strict: 
+                cols[col] = f"%{cols[col]}%"
+
+    year = request.form.get('year')
+    # prefill or db.execute will complain aout unbound variables
+    year_from, year_to = 0, 0
+    
+    if year != '':
+        years = year.split('-')
+        year_from = years[0].strip()
+        year_to = years[1].strip() if len(years)>1 else year_from
+        
+        where_clause.append("books.year >= :year_from AND books.year <= :year_to")
+    
+    if len(where_clause) == 0:
+        errors.append("Please fill at least one search field.")
+        return render_template('search.html', errors=errors)
+
+    where_clause = ' AND '.join(where_clause)
+    sql = '''   SELECT books.id, title, year, authors.author, isbn FROM books 
+                RIGHT JOIN authors ON books.author_id = authors.id
+                WHERE ''' + where_clause + ' ORDER BY title ASC'
+
+    books = db.execute(sql, {'title':   cols['title'], 
+                            'author':   cols['author'], 
+                            'isbn':     cols['isbn'],
+                            'year_from':year_from, 
+                            'year_to':  year_to}).fetchall()
+    if len(books) == 0:
+        errors.append("No books found.")
+        return render_template('search.html', errors=errors)
+    else:
+        return render_template('book_results.html', books=books)
+    
 
 # Book details
 @app.route("/book/<int:book_id>")
